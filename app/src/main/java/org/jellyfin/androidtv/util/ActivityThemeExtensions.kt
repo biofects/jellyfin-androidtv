d7cdae3e781cdc5e05c2d91b213fa98fb90ba70b
tfam <biofects@gmail.com>
2026-08-28 09:03:59 -0500

    Add Enhanced Biofects app theme

--- app/src/main/java/org/jellyfin/androidtv/util/ActivityThemeExtensions.kt
+++ app/src/main/java/org/jellyfin/androidtv/util/ActivityThemeExtensions.kt
@@ -17,6 +17,7 @@ private val AppTheme.style
 		AppTheme.DARK -> R.style.Theme_Jellyfin
 		AppTheme.EMERALD -> R.style.Theme_Jellyfin_Emerald
 		AppTheme.MUTED_PURPLE -> R.style.Theme_Jellyfin_MutedPurple
+		AppTheme.ENHANCED_BIOFECTS -> R.style.Theme_Jellyfin_EnhancedBiofects
 	}
 
 /**
